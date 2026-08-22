29579b3910dd587d339dbd53763001f28ea1bba3
Domanic <2889884142@qq.com>
2026-08-22 15:48:56 +0800

    Add network configuration template

---
+++ secrets.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// 修改为你的 2.4 GHz Wi-Fi。
+#define WIFI_SSID     "YOUR_WIFI_NAME"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+// Raspberry Pi G8 Controller
+#define G8_API_BASE   "http://YOUR_PI_IP:5000"
